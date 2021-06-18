47c8ab24841b6a8ef82081e71a449f6b855ce577
CharmsLUO <704097440@qq.com>
2021-06-18 19:27:48 +0800

    Add files via upload

---
+++ train.py
@@ -0,0 +1,30 @@
+"""
+TRAIN SKIP/GANOMALY
+
+. Example: Run the following command from the terminal.
+    run train.py                                    \
+        --model <skipganomaly, ganomaly>            \
+        --dataset cifar10                           \
+        --abnormal_class airplane                   \
+        --display                                   \
+"""
+
+##
+# LIBRARIES
+
+from options import Options
+from lib.data.dataloader import load_data
+from lib.models import load_model
+
+##
+def main():
+    """ Training
+    """
+    opt = Options().parse()
+    data = load_data(opt)
+    model = load_model(opt, data)
+    model.train()
+    # model.test()
+
+if __name__ == '__main__':
+    main()
